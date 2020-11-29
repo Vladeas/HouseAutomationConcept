e7aa4b417f7f2084e34adca732970c0449f14d78
vladeas <vladeas@users.noreply.github.com>
2020-11-29 13:17:22 +0200

    0.0.1 GUI Class Experimantation

--- GUI.py
+++ GUI.py
@@ -1,12 +1,19 @@
 from tkinter import *
 
-class MainAppGUI():
+#Menu Bar Frame Added to the Main Frame
+class MenuBarGUI():
     def __init__(self, parent):
         self.parent = parent
-        
-        self.parent.title("Home Hub")
-        self.parent.geometry("1600x900")
-        _create_action_bar()
+        self.homeButton = Button(self.actionBar, text = "Home")
+
+#Main Frame, Window of the App
+class MainAppGUI():
+    def __init__(self, parent, resolution, appTitle):
+        self.parent = parent
+
+        self.parent.title(appTitle)
+        self.parent.geometry(resolution)
+
 
     def _create_action_bar():
         self.actionBar = LabelFrame(self.parent)
@@ -17,9 +24,21 @@ class MainAppGUI():
         self.settingsButton.grid(row = 0, column = 0)
         self.helpButton.grid(row = 0, column = 1)
 
+#Get the resolution of the current device screen
+#Return it in a format to be used witk tkinter
+def _get_screen_resolution(parent):
+    width = parent.winfo_screenwidth()
+    height = parent.winfo_screenheight()
+    return str(width) + "x" + str(height) 
+
+#Main Function of the GUI -->> Here it starts
+#Needs to be called to Create the GUI
 def start_GUI():
     root = Tk()
 
-    app = MainAppGUI(root)
+    resolution = _get_screen_resolution(root)
+    appTitle = "Home Hub"
+
+    app = MainAppGUI(root, resolution, appTitle)
 
     root.mainloop()
